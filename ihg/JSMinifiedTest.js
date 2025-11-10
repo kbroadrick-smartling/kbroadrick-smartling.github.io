190550676a4ec998625fc0fec55f90abef0377df
kbroadrick-smartling <kbroadrick@smartling.com>
2025-11-10 08:31:35 -0600

    Create JSMinifiedTest.js

---
+++ ihg/JSMinifiedTest.js
@@ -0,0 +1,23 @@
+// Smartling directive function declarations (required to survive minification)
+function sl_translate() {}
+
+sl_tr_start     = sl_translate;
+sl_tr_end       = sl_translate;
+sl_notr_start   = sl_translate;
+sl_notr_end     = sl_translate;
+sl_tr_html_start = sl_translate;
+sl_tr_html_end   = sl_translate;
+sl_tr_json_start = sl_translate;
+sl_tr_json_end   = sl_translate;
+
+// ► Smartling translation block ◄
+sl_tr_start();
+
+const me = {
+  enData: {
+    LO_TITLE: "KB Log Out Alert",
+    LO_MESSAGE: "KB You have been logged out due to inactivity",
+    LO_CONTINUE: "KB Go to Login Screen"
+  }
+};
+sl_tr_end();
